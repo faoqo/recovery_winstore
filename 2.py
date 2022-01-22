b6a960efbfd43a0ad97888cc86d7f78fb5b22f55
faoqo <dacmapro@gmail.com>
2022-01-22 18:25:25 +0300

    1

--- 2.py
+++ 2.py
@@ -1,5 +1,7 @@
 import zipfile
 import platform
+import subprocess
+
 win_vers = platform.architecture()
 Win_64_text = "('64bit', 'WindowsPE')"
 Win_32_text = "('32bit', 'WindowsPE')"
@@ -10,6 +12,12 @@ if str_winvers == Win_64_text:
     fantasy_zip.extractall('C:\\Ps\\Store')
     fantasy_zip.close()
     print ("unpack 64 to C")
+    proc = subprocess.Popen(['powershell', 'cd C:\PS\Store'])
+    proc.wait()
+    proc = subprocess.Popen(['powershell', 'Get-Childitem $Path -filter *.appx| %{Add-AppxPackage -Path $_.FullName}'])
+    proc.wait()
+    proc = subprocess.Popen(['powershell', 'Get-Childitem $Path -filter *.appxbundle | %{Add-AppxPackage -Path $_.FullName}'])
+    proc.wait()
 
 elif str_winvers == Win_32_text:
     fantasy_zip = zipfile.ZipFile('pack_32.zip')
--- test.py
+++ test.py
@@ -1,21 +1,8 @@
-import zipfile
-import platform
-win_vers = platform.architecture()
-Win_64_text = "('64bit', 'WindowsPE')"
-Win_32_text = "('32bit', 'WindowsPE')"
-str_winvers = str(win_vers)
+import subprocess
 
-if str_winvers == Win_64_text:
-    fantasy_zip = zipfile.ZipFile('pack_64.zip')
-    fantasy_zip.extractall('C:\\Ps\\Store')
-    fantasy_zip.close()
-    print ("unpack 64 to C")
-
-elif str_winvers == Win_32_text:
-    fantasy_zip = zipfile.ZipFile('pack_32.zip')
-    fantasy_zip.extractall('C:\\Ps\\Store')
-    fantasy_zip.close()
-    print ("unpack 32 to C")
-    
-else:
-    print("error win version")
+proc = subprocess.Popen(['powershell', 'cd C:\PS\Store'])
+proc.wait()
+proc = subprocess.Popen(['powershell', 'Get-Childitem $Path -filter *.appx| %{Add-AppxPackage -Path $_.FullName}'])
+proc.wait()
+proc = subprocess.Popen(['powershell', 'Get-Childitem $Path -filter *.appxbundle | %{Add-AppxPackage -Path $_.FullName}'])
+proc.wait()
